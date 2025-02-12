9e59b7627864473fec245fed11bb55d2990c36f8
Drew McDonald <drew@watershedclimate.com>
2025-02-12 01:42:52 +0000

    lints

--- flipdot/display_mode/__init__.py
+++ flipdot/display_mode/__init__.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from pydantic import BaseModel
 
 from flipdot.display_mode.BaseDisplayMode import BaseDisplayMode, DisplayModeRef
--- flipdot/vend/__init__.py
+++ flipdot/vend/__init__.py
@@ -1,1 +1,3 @@
 from .flippydot import Panel
+
+__all__ = ["Panel"]
